9ea5116461f4b6eee1a44c389d89e97f93e6c2d5
Márcio Meier <marciohmeier97@gmail.com>
2019-10-31 20:23:47 -0300

    controllers

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { UserCollection } from './routes/User'
 import { SectorCollection } from './routes/Sector'
 import { Collection } from './routes/Collection'
 import { NotificationCollection } from './routes/Notification';
+import { MenuItemCollection } from './routes/MenuItem';
 
 export class Routes {
   private collections : Array<Collection>;
@@ -12,6 +13,7 @@ export class Routes {
     this.addCollection(new UserCollection())
     this.addCollection(new SectorCollection())
     this.addCollection(new NotificationCollection())
+    this.addCollection(new MenuItemCollection())
   }
 
   public getCollections(): Array<Collection> {
