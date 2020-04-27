01e4fdfa0bdac890d0d7a80284ac7ebd111c215a
Márcio Meier <marciohmeier97@gmail.com>
2020-04-27 20:37:20 -0300

    adjustes endpoint gets added end point create and update order

--- src/controllers/MaintenanceOrder.ts
+++ src/controllers/MaintenanceOrder.ts
@@ -1,11 +1,13 @@
-import { getRepository, Repository, getConnection, SelectQueryBuilder } from "typeorm";
+import { getRepository, Repository, SelectQueryBuilder } from "typeorm";
+import { validate } from "class-validator";
 import { MaintenanceOrder } from "../models/maintenance-order/MaintenanceOrder";
-import {NextFunction, Request, Response} from "express";
+import { NextFunction, Request, Response } from "express";
+import * as jwt from "jsonwebtoken";
+import JWT from "../config/JWT";
 
 export class MaintenanceOrderController {
 
   private repositoryEntity : Repository<MaintenanceOrder>;
-  private connection = getConnection();
 
   constructor() {
     this.repositoryEntity = getRepository(MaintenanceOrder)
@@ -27,7 +29,9 @@ export class MaintenanceOrderController {
           maintenanceWorker: 'order.maintenanceWorker',
         },
         leftJoinAndSelect: {
-          maintenanceEquipment: 'order.orderEquipment'
+          maintenanceEquipment: 'order.orderEquipment',
+          orderLayout: 'order.orderLayout',
+          equipment : 'maintenanceEquipment.equipment',
         }
       },
       where: (qb: SelectQueryBuilder<MaintenanceOrder>) => {
@@ -46,7 +50,7 @@ export class MaintenanceOrderController {
 
     const arrayOfOrders = await this.getRepositoryEntity().find({
       select: <any>this.fieldsResume(),
-      relations: ['orderEquipment', 'maintenanceWorker', 'maintenanceWorker.user'],
+      relations: ['orderLayout', 'orderEquipment', 'orderEquipment.equipment', 'maintenanceWorker', 'maintenanceWorker.user'],
       where: { deleted: false }
     });
 
@@ -65,15 +69,124 @@ export class MaintenanceOrderController {
   }
   
   public async createOrder(request: Request, response: Response, next: NextFunction) {
+    const order = this.getRepositoryEntity().create(<MaintenanceOrder>request.body);
+
+    const token = <string>request.headers["token"];
+    this.updateFields(token, order);
+
     
+
+    //Validade if the parameters are ok
+    const error = await this.validate(order)
+    if (error !== undefined) {
+      return {
+        success: false,
+        error: error
+      }
+    }
+    
+    if (order["integrationID"] != "") {
+      try {
+        await this.getRepositoryEntity().findOneOrFail({where: {integrationID: order["integrationID"]}});
+        return {"success":false,"error":`Registro com o integrationID ${order["integrationID"]} já existe.`};
+      } catch (error) {
+        // Não está duplicado
+      }
+    }
+
+    await this.getRepositoryEntity().save(order);
   }
   
   public async updateOrder(request: Request, response: Response, next: NextFunction) {
-    
+    let order: MaintenanceOrder
+
+    try {
+      order = await this.getRepositoryEntity().findOneOrFail(request.params.id);
+      order = this.getRepositoryEntity().merge(order, request.body)
+    } catch (error) {
+      return { "success":false, "error":`Ordem ${request.params.id} não encontrada` };
+    }
+
+    const token = <string>request.headers["token"];
+    this.updateFields(token, order);
+
+    const errors = await validate(order);
+    if (errors.length > 0) {
+      return {
+        "success":false,
+        "error":errors
+      };
+    }
+
+    let result = await this.getRepositoryEntity().update(request.params.id,order)
+    if (!result) return { "success":false, "error":`Erro ao executar a atualização da ordem ${request.params.id}` }
+
+    return await this.getRepositoryEntity().findOne(request.params.id, {relations: this.getAllRelations()});
   }
   
   public async deleteOrder(request: Request, response: Response, next: NextFunction) {
+    let order: MaintenanceOrder
+
+    try {
+      order = await this.getRepositoryEntity().findOneOrFail(request.params.id, {relations: this.getAllRelations()});
+    } catch (error) {
+      return { "success":false, "error":`Ordem ${request.params.id} não encontrada` };
+    }
+
+    if (order["deleted"] === true) {
+      return { "success":false, "error":`Ordem ${request.params.id} já está excluída` };
+    }
+
+    const token = <string>request.headers["token"];
+    this.updateFields(token, order);
+
+    order["setDeleted"](true);
+    const errors = await validate(order);
+    if (errors.length > 0) {
+      return {
+        "success":false,
+        "error":errors
+      };
+    }
+
+    let result = await this.getRepositoryEntity().update(request.params.id,order)
+    if (!result) return { "success":false, "error":"Erro ao deletar a ordem" }
+
+
+
+    return await this.getRepositoryEntity().findOne(request.params.id);
+  }
+
+  updateFields(token:string, entity : MaintenanceOrder) {
+    let jwtPayload = <any>jwt.verify(token, JWT.jwtSecret);
+    const { userId } = jwtPayload;
+
+    entity["updatedBy"] = userId;
+    if (entity["createdBy"] === undefined) {
+      entity["createdBy"] = userId;
+    }
+  }
+
+  async validate(entity: MaintenanceOrder) : Promise<any> {
+    const errors = await validate(entity);
+
+    if (errors.length === 0) {
+      return undefined
+    }
     
+    let errorList = []
+
+    errors.forEach(error => {
+      let constraints = error.constraints
+
+      for (const key in constraints) {
+        if (constraints.hasOwnProperty(key)) {
+          errorList.push(constraints[key])
+        }
+      }
+    });
+
+    return errorList
   }
 
   public fieldsResume() {
