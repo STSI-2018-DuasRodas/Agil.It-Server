22db0b48ce7adabb8b415d686b0fa7c679a765da
Márcio Meier <marciohmeier97@gmail.com>
2020-09-21 19:50:50 -0300

    dashboards admin

---
+++ src/controllers/Dashboards.ts
@@ -0,0 +1,22 @@
+import { OrderStatus } from './../models/enum/OrderStatus';
+import { MaintenanceOrderController } from './MaintenanceOrder';
+import { Request, Response, NextFunction } from "express";
+
+export class DashboardsController {
+
+
+  public async getAdminDashboard(request: Request, response: Response, next: NextFunction) {
+    // route: [GET] dashboards/admin
+
+    return new MaintenanceOrderController()
+      .getRepositoryEntity()
+      .createQueryBuilder('maintenanceOrder')
+      .leftJoinAndSelect('maintenanceOrder.orderSignature', 'orderSignature')
+      .where('maintenanceOrder.deleted = :deleted', { deleted: false })
+      .andWhere('orderSignature.deleted = :signatureDeleted', { signatureDeleted: false })
+      .andWhere('maintenanceOrder.exported = :exported', { exported: false })
+      .andWhere('maintenanceOrder.orderStatus in (:...statuses)', { statuses: [OrderStatus.SIGNATURED, OrderStatus.SIGNATURE_PENDING, OrderStatus.FINISHED] })
+      .getMany();
+  }
+
+}
---
+++ src/routes/Dashboards.ts
@@ -0,0 +1,17 @@
+import { DashboardsController } from "../controllers/Dashboards";
+import { Collection } from "./Collection";
+import { Route } from "./Route";
+import { Method } from "./Method";
+
+export class DashboardsCollection extends Collection {
+  constructor() {
+    super('/dashboards', DashboardsController);
+
+    this.addRoute(new Route(
+      Method.GET,
+      `${this.getRoute()}/admin`,
+      this.getController(),
+      'getAdminDashboard'
+    ))
+  }
+}
